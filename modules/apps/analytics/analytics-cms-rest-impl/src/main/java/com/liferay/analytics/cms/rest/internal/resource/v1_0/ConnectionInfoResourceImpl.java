ac2297ccaf0970cf123f65e8fdc61aa7a9714265
Leslie Wong <8095751+lesliewong92@users.noreply.github.com>
2025-07-15 16:03:31 -0700

    LPD-43294 | LPD-59962 Implement getConnectionInfo

--- modules/apps/analytics/analytics-cms-rest-impl/src/main/java/com/liferay/analytics/cms/rest/internal/resource/v1_0/ConnectionInfoResourceImpl.java
+++ modules/apps/analytics/analytics-cms-rest-impl/src/main/java/com/liferay/analytics/cms/rest/internal/resource/v1_0/ConnectionInfoResourceImpl.java
@@ -5,9 +5,22 @@
 
 package com.liferay.analytics.cms.rest.internal.resource.v1_0;
 
+import com.liferay.analytics.cms.rest.dto.v1_0.ConnectionInfo;
 import com.liferay.analytics.cms.rest.resource.v1_0.ConnectionInfoResource;
+import com.liferay.analytics.settings.configuration.AnalyticsConfiguration;
+import com.liferay.analytics.settings.rest.manager.AnalyticsSettingsManager;
+import com.liferay.depot.model.DepotEntry;
+import com.liferay.depot.model.DepotEntryGroupRelModel;
+import com.liferay.depot.service.DepotEntryGroupRelLocalService;
+import com.liferay.depot.service.DepotEntryService;
+import com.liferay.portal.kernel.model.role.RoleConstants;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -18,4 +31,69 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = ConnectionInfoResource.class
 )
 public class ConnectionInfoResourceImpl extends BaseConnectionInfoResourceImpl {
+
+	@Override
+	public ConnectionInfo getConnectionInfo(Long spaceId) throws Exception {
+		AnalyticsConfiguration analyticsConfiguration =
+			_analyticsSettingsManager.getAnalyticsConfiguration(
+				contextUser.getCompanyId());
+
+		List<Long> groupIds = _getDepotEntryGroupRelToGroupId(
+			_depotEntryService.getDepotEntry(spaceId));
+
+		return _toConnectionInfo(
+			roleLocalService.hasUserRole(
+				contextUser.getUserId(), contextUser.getCompanyId(),
+				RoleConstants.ADMINISTRATOR, true),
+			!Validator.isBlank(analyticsConfiguration.token()),
+			!groupIds.isEmpty(),
+			_hasSitesSyncedToAnalyticsCloud(
+				analyticsConfiguration.syncedGroupIds(), groupIds));
+	}
+
+	private List<Long> _getDepotEntryGroupRelToGroupId(DepotEntry depotEntry) {
+		return transform(
+			_depotEntryGroupRelLocalService.getDepotEntryGroupRels(depotEntry),
+			DepotEntryGroupRelModel::getToGroupId);
+	}
+
+	private boolean _hasSitesSyncedToAnalyticsCloud(
+		String[] analyticsCloudSyncedGroupIds, List<Long> groupIds) {
+
+		for (long groupId : groupIds) {
+			if (ArrayUtil.contains(
+					analyticsCloudSyncedGroupIds, String.valueOf(groupId))) {
+
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	private ConnectionInfo _toConnectionInfo(
+		boolean admin, boolean connectedToAnalyticsCloud,
+		boolean connectedToSpace, boolean siteSyncedToAnalyticsCloud) {
+
+		ConnectionInfo connectionInfo = new ConnectionInfo();
+
+		connectionInfo.setAdmin(() -> admin);
+		connectionInfo.setConnectedToAnalyticsCloud(
+			() -> connectedToAnalyticsCloud);
+		connectionInfo.setConnectedToSpace(() -> connectedToSpace);
+		connectionInfo.setSiteSyncedToAnalyticsCloud(
+			() -> siteSyncedToAnalyticsCloud);
+
+		return connectionInfo;
+	}
+
+	@Reference
+	private AnalyticsSettingsManager _analyticsSettingsManager;
+
+	@Reference
+	private DepotEntryGroupRelLocalService _depotEntryGroupRelLocalService;
+
+	@Reference
+	private DepotEntryService _depotEntryService;
+
 }
